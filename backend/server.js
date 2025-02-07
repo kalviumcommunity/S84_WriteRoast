54bb493fa9ec4e0a7b38517b4252bd3566a06fa1
DIVEN009 <sainidiven@gmail.com>
2025-02-07 11:07:48 +0530

    added

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-// Define the /ping route
+app.get("/", (req, res) => {
+    res.send("Welcome to my Express server!");
+});
+
 app.get("/ping", (req, res) => {
     res.send("Pong!");
 });
 
-// Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Hi, my name is Diven Saini. Server is running on http://localhost:${PORT}`);
 });
